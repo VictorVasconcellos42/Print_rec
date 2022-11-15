93a0f923cadb83e21742a8da5a7667dc7423d756
vde-vasc <vde-vasc@student.42.rio>
2022-11-14 22:11:05 -0300

    header created

---
+++ includes/header.h
@@ -0,0 +1,12 @@
+
+#ifndef HEADER_H
+# define HEADER_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include "utils/libft/libft.h"
+# include "utils/get_next_line/get_next_line.h"
+
+#endif
