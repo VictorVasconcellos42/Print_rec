3f58ffff47f91c5175007fd629b1429482280b34
vde-vasc <vde-vasc@student.42.rio>
2022-11-16 20:34:02 -0300

    New header path to utils files

--- includes/header.h
+++ includes/header.h
@@ -6,7 +6,7 @@
 # include <stdlib.h>
 # include <string.h>
 # include <unistd.h>
-# include "utils/libft/libft.h"
-# include "utils/get_next_line/get_next_line.h"
+# include "../utils/libft/libft.h"
+# include "../utils/get_next_line/get_next_line.h"
 
 #endif
